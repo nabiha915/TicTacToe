c0ea4655e1975b0a36d41df893c64749a5b2b8e7
Nabiha Ahmed <55366031+nabiha915@users.noreply.github.com>
2021-09-21 14:18:03 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+     
+     //creating ttt object that belongs to the TicTacToe class
+     TicTacToe ttt = new TicTacToe();
+
+
+    //using the ttt object to call the setup method 
+     ttt.setup();
+  
+  }
+}
